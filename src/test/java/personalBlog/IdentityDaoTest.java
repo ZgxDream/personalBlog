41c02d4bfc491e25ce3179b44772c061dfb363e2
Admin <Admin@AirBook>
2017-03-09 00:24:10 +0800

    【增加】：identity的mapper文件

---
+++ src/test/java/personalBlog/IdentityDaoTest.java
@@ -0,0 +1,43 @@
+package personalBlog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cn.edu.zut.personalBlog.dao.IdentityDao;
+import cn.edu.zut.personalBlog.entity.Identity;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+
+@ContextConfiguration({"classpath:conf/spring-sql.xml"  })
+public class IdentityDaoTest {
+@Autowired
+ private IdentityDao identityDao;
+
+ @Test
+ public void test(){
+	 int id=3;
+     identityDao.selectInfoById(id);
+	 System.out.println(identityDao.toString());
+	 
+ }
+ 
+
+
+//@Test
+//public void test2(){
+//   
+//	Identity id=new Identity();
+//	id.setId(3);
+//	id.setName("ËÎöÌöÎ");
+//	id.setSex(0);
+//	id.setPhone("333333");
+//	id.setQq_num("21412");
+//	id.setResume("ÇÚÀÍµÄÐ¡ÃÛ·ä");
+//	identityDao.insert(id);
+//	System.err.println(identityDao.toString());
+//
+//}
+}
