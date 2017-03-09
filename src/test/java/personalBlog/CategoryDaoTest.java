456ab060c47e816927c82a2bb3fc5e09f8ef619c
Admin <Admin@AirBook>
2017-03-09 09:32:38 +0800

    【修改】：Category的测试与mapper，Logtable的mapper

---
+++ src/test/java/personalBlog/CategoryDaoTest.java
@@ -0,0 +1,57 @@
+package personalBlog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cn.edu.zut.personalBlog.dao.CategoryDao;
+import cn.edu.zut.personalBlog.entity.Category;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+
+@ContextConfiguration({"classpath:conf/spring-sql.xml"  })
+public class CategoryDaoTest {
+	@Autowired
+	private CategoryDao categroyDao;
+	
+//	@Test
+//	public void test(){
+//		
+//		Category category=new Category();
+//		category.setId(5);
+//		category.setCa_name("c++");
+//		category.setCa_resume("±‡≥Ã”Ô—‘");
+//		categroyDao.addCatById(category);
+//		System.out.println(category.toString());
+//	}
+//	
+	
+//	@Test
+//    public void test1(){
+//		Category category=new Category();
+//		category.setId(2);
+//	    category.setCa_resume("yuyn");
+//	    categroyDao.updateBlogById(category);
+//	    System.out.println(category.toString());
+//	
+//}
+//	@Test
+//	public void test2(){
+//		
+//		Category category=new Category();
+//		int id=4;
+//	   
+//	    System.out.println( categroyDao.deleteCateById(id));
+//		
+//	}
+	@Test
+	public void test3(){
+		Category category=new Category();
+		int id=1;
+		System.out.println(categroyDao.selectAllCat(id));
+		
+		
+	}
+}
